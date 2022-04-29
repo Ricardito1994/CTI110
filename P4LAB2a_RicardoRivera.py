c9336ef3a54eba1d0d1dcb2f1098a7f73158e119
Ricardito1994 <99997438+Ricardito1994@users.noreply.github.com>
2022-04-29 19:23:22 -0400

    LAB submission 9

---
+++ P4LAB2a_RicardoRivera.py
@@ -0,0 +1,24 @@
+#Square
+import turtle
+win = turtle.Screen()
+Rick = turtle.Turtle()
+
+Rick.forward(50)
+Rick.left(90)
+Rick.forward(50)
+Rick.left(90)
+Rick.forward(50)
+Rick.left(90)
+Rick.forward(50)
+Rick.penup()
+Rick.forward(150)
+Rick.pendown()
+
+#Triangle
+Rick.forward(100)
+Rick.left(120)
+Rick.forward(100)
+Rick.left(120)
+Rick.forward(100)
+
+win.mainloop()
---
+++ P4LAB2b_RicardoRivera.py
@@ -0,0 +1,49 @@
+#First Initial
+import turtle
+win = turtle.Screen()
+ricardo = turtle.Turtle()
+
+ricardo.left(90)
+ricardo.forward(90)
+ricardo.right(120)
+ricardo.forward(50)
+ricardo.right(120)
+ricardo.forward(50)
+ricardo.left(110)
+ricardo.forward(65)
+ricardo.penup()
+ricardo.left(35)
+ricardo.forward(50)
+ricardo.pendown()
+
+#Middle Initial
+ricardo.left(70)
+ricardo.forward(90)
+ricardo.right(130)
+ricardo.forward(90)
+ricardo.right(180)
+ricardo.penup()
+ricardo.forward(45)
+ricardo.pendown()
+ricardo.left(65)
+ricardo.forward(37)
+ricardo.penup()
+ricardo.right(180)
+ricardo.forward(100)
+ricardo.right(90)
+ricardo.forward(45)
+ricardo.left(180)
+ricardo.pendown()
+
+#Last Initial
+ricardo.forward(90)
+ricardo.right(120)
+ricardo.forward(50)
+ricardo.right(120)
+ricardo.forward(50)
+ricardo.left(110)
+ricardo.forward(65)
+
+win.mainloop()
+
+
