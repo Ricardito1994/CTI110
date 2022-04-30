824f2ee91f7ce572d051788e891efa6edb5dcdea
Ricardito1994 <99997438+Ricardito1994@users.noreply.github.com>
2022-04-29 21:14:48 -0400

    Delete P5LAB1_RicardoRivera.py

--- P5LAB1_RicardoRivera.py
+++
@@ -1,7 +0,0 @@
-def laps_to_miles(user_laps):
-   return user_laps/4
-    
-if __name__=='__main__':
-    user_laps = float(input())
-    your_value = laps_to_miles(user_laps)
-    print(f'{your_value:.2f}')
